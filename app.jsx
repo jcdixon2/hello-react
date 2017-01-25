a3c66178dae12316c073da010f48ab65f1683820
Jack Dixon <jcdixon2@gmail.com>
2017-01-24 23:47:42 -0600

    Completed Nested Components

--- app.jsx
+++ app.jsx
@@ -1,9 +1,13 @@
 var GreeterMessage = React.createClass({
+
   render: function() {
+    var name = this.props.name;
+    var message = this.props.message;
+
     return (
       <div>
-        <h1>Some H1</h1>
-        <p>Some paragraph</p>
+        <h1>Hello React Greeter {name}!!</h1>
+        <p>{message + "!!"}</p>
       </div>
     )
   }
@@ -60,10 +64,7 @@ var Greeter = React.createClass({
 
     return(
       <div>
-        <h1>Hello React Greeter {name}!!</h1>
-        <p>{message + "!!"}</p>
-
-        <GreeterMessage name={name}/>
+        <GreeterMessage name={name} message={message}/>
         <GreeterForm onNewName={this.handleNewName}/>
       </div>
     );
