ffac03b571b6bed922a31f9d9484335d06c4b9c4
Jack Dixon <jcdixon2@gmail.com>
2017-01-24 20:15:11 -0600

    Initial Commit

---
+++ app.jsx
@@ -0,0 +1,48 @@
+var Greeter = React.createClass({
+  getDefaultProps: function(){
+    return {
+      name: "React",
+      message: "my simple message"
+    };
+  },
+  getInitialState: function(){
+    return {
+      name: this.props.name
+    };
+  },
+  onButtonClick: function(e){
+    e.preventDefault();
+
+    var nameRef = this.refs.name;
+    var name = nameRef.value;
+    nameRef.value = "";
+    //alert(name);
+
+    if (typeof name === "string" && name.length > 0)
+    {
+      this.setState({
+        name: name
+      });
+    }
+  },
+  render: function(){
+    var name = this.state.name
+    return(
+      <div>
+        <h1>Hello React Greeter {name}!!</h1>
+        <p>{this.props.message}</p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"/>
+          <button>Set Name</button>
+        </form>
+      </div>
+    );
+  }
+});
+
+var firstName = "Jack";
+ReactDOM.render(
+  <Greeter name={firstName} message="hello jack"/>,
+  document.getElementById("app")
+);
